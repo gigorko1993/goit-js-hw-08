b179ca7fd1a91f0800346b4fffa8010b2f1e22df
gigorko1993 <gigorko1993@gmail.com>
2021-04-25 19:29:16 +0300

    finished 08 hw

---
+++ js/hw-08.js
@@ -0,0 +1,73 @@
+import markupElementsArray from '../gallery-items.js';
+
+const listElements = document.querySelector('.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const overlayImageEl = document.querySelector('.lightbox__image');
+const listMarkup = makeMarkup(markupElementsArray);
+
+listElements.addEventListener('click', onImageClick)
+lightbox.addEventListener('click', onCloseModal)
+
+listElements.insertAdjacentHTML('beforeend', listMarkup);
+
+function makeMarkup(array) {
+    const markup = array.map(({ original, preview, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li> 
+  `}).join('')
+    return markup;
+};
+ 
+function getArrayOfImageURL(arr) {
+    const arrayOfImgUrl = arr.map(a => a.original);
+    return arrayOfImgUrl;
+};
+ 
+function onImageClick(evt) {
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    };
+
+    evt.preventDefault();
+    window.addEventListener('keydown', onEscButtonPress)
+    window.addEventListener('keydown', onArrowButtonPress)
+    lightbox.classList.add('is-open');
+    overlayImageEl.src = evt.target.dataset.source;
+};
+ 
+function onCloseModal(evt) {
+    if (evt.target.dataset.action === "close-lightbox" || evt.target.classList.value === "lightbox__overlay" || evt.code === 'Escape') {
+        lightbox.classList.remove('is-open');
+        overlayImageEl.src = "";
+        window.removeEventListener('keydown', onEscButtonPress)
+        window.removeEventListener('keydown', onArrowButtonPress)
+    }
+}
+
+function onEscButtonPress(evt) {
+    if (evt.code === 'Escape') {
+        lightbox.classList.remove('is-open');
+        overlayImageEl.src = "";
+        window.removeEventListener('keydown', onEscButtonPress)
+        window.removeEventListener('keydown', onArrowButtonPress)
+    }
+}
+function onArrowButtonPress(evt) {
+    if (evt.code === 'ArrowRight' || evt.code === 'ArrowLeft') {
+        const arrayOfUrl = getArrayOfImageURL(markupElementsArray);
+        let indexInArray = arrayOfUrl.indexOf(overlayImageEl.src);
+
+        evt.code === 'ArrowLeft'
+            ? overlayImageEl.src = arrayOfUrl[indexInArray - 1]
+            : overlayImageEl.src = arrayOfUrl[indexInArray + 1];
+    }
+    }
